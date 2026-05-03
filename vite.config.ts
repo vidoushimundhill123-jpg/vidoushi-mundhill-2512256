d2b2b00ec9bce00b6dac18ade006b0a39f690005
vidoushimundhill123-jpg <vidoushimundhill123@gmail.com>
2026-05-03 18:34:39 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/portfolio-2026/',
+    base: '/porfolio-2026/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
